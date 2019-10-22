cc95fba28eafec869b1bf70736ec213babb2a1dd
Mehdi Rahmani <mehdi.rahmani@cern.ch>
2019-10-22 17:42:50 -0400

    v3

--- Matching_muons_v1.cxx
+++ Matching_muons_v1.cxx
@@ -53,11 +53,35 @@ int Matching_muons_v1(){
     Float_t genMu3_phi;
 
 
+
+
+
     TFile *myFile = new TFile("ALL.root");
-    TCanvas *cnv = new TCanvas();
-    TH1F *sD_Lxy = new TH1F("dR", "dR", 100, -1E-4, 1E-2);
-    sD_Lxy->SetXTitle("dR");
-    sD_Lxy->SetYTitle("Number of Events");
+
+    
+    
+    
+    
+    TCanvas *cnv1 = new TCanvas();
+    TH1F *sD_Lxy1 = new TH1F("dR1", "dR1", 100, -1E-4, 5E-2);
+
+    TCanvas *cnv2 = new TCanvas();
+    TH1F *sD_Lxy2 = new TH1F("dR2", "dR2", 100, -1E-4, 5E-2);
+
+    TCanvas *cnv3 = new TCanvas();
+    TH1F *sD_Lxy3 = new TH1F("dR3", "dR3", 100, -1E-4, 5E-2);
+
+    TCanvas *cnv4 = new TCanvas();
+    TH1F *sD_Lxy4 = new TH1F("dR4", "dR4", 100, -1E-4, 5E-2);
+
+    sD_Lxy1->SetXTitle("dR (best match for selMu0)");
+    sD_Lxy1->SetYTitle("Number of Events");
+    sD_Lxy2->SetXTitle("dR (best match for selMu1)");
+    sD_Lxy2->SetYTitle("Number of Events ");
+    sD_Lxy3->SetXTitle("dR (best match for selMu2)");
+    sD_Lxy3->SetYTitle("Number of Events");
+    sD_Lxy4->SetXTitle("dR(best match for selMu3)");
+    sD_Lxy4->SetYTitle("Number of Events");
 
     gDirectory->cd("cutFlowAnalyzerPXBL4PXFL3;1");
     gDirectory->pwd();
@@ -96,12 +120,43 @@ int Matching_muons_v1(){
 
             myTree1->GetEntry(ii);
             
-            Float_t a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, min;
-
+            Float_t a, b, c, d, min1;
+            min1= min(min(min(a,b), c),d);
+          
             if (    
             selMu0_eta != -100 &&
             genMu0_eta != -100 && 
             selMu0_phi != -100 &&
+            genMu0_phi != -100 &&
+            
+            genMu1_eta != -100 &&
+            genMu1_phi != -100 &&
+
+            
+            genMu2_eta != -100 &&
+            genMu2_phi != -100 &&
+
+            
+            genMu3_eta != -100 &&   
+            genMu3_phi != -100 ){
+
+            
+            
+            a = ( (selMu0_eta - genMu0_eta) * (selMu0_eta - genMu0_eta) ) +  ( (selMu0_phi - genMu0_phi) * (selMu0_phi - genMu0_phi) ) ;
+            b = ( (selMu0_eta - genMu1_eta) * (selMu0_eta - genMu1_eta) ) +  ( (selMu0_phi - genMu1_phi) * (selMu0_phi - genMu1_phi) ) ;
+            c = ( (selMu0_eta - genMu2_eta) * (selMu0_eta - genMu2_eta) ) +  ( (selMu0_phi - genMu2_phi) * (selMu0_phi - genMu2_phi) ) ;
+            d = ( (selMu0_eta - genMu3_eta) * (selMu0_eta - genMu3_eta) ) +  ( (selMu0_phi - genMu3_phi) * (selMu0_phi - genMu3_phi) ) ;
+            
+            
+            sD_Lxy1->Fill(sqrt(min1));   
+            }
+
+            Float_t e, f, g, h, min2;
+            min2 = std::min(std::min(std::min(e,f), g),h);
+    
+            if (    
+          
+            genMu0_eta != -100 && 
             genMu0_phi != -100 &&
 
             selMu1_eta != -100 &&
@@ -109,9 +164,81 @@ int Matching_muons_v1(){
             selMu1_phi != -100 &&
             genMu1_phi != -100 &&
 
+ 
+            genMu2_eta != -100 &&
+            genMu2_phi != -100 &&
+
+            genMu3_eta != -100 &&
+            genMu3_phi != -100 ){
+
+            
+
+            e = ( (selMu1_eta - genMu0_eta) * (selMu1_eta - genMu0_eta) ) +  ( (selMu1_phi - genMu0_phi) * (selMu1_phi - genMu0_phi) ) ;
+            f = ( (selMu1_eta - genMu1_eta) * (selMu1_eta - genMu1_eta) ) +  ( (selMu1_phi - genMu1_phi) * (selMu1_phi - genMu1_phi) ) ;
+            g = ( (selMu1_eta - genMu2_eta) * (selMu1_eta - genMu2_eta) ) +  ( (selMu1_phi - genMu2_phi) * (selMu1_phi - genMu2_phi) ) ;
+            h = ( (selMu1_eta - genMu3_eta) * (selMu1_eta - genMu3_eta) ) +  ( (selMu1_phi - genMu3_phi) * (selMu1_phi - genMu3_phi) ) ;
+            sD_Lxy2->Fill(sqrt(min2)); 
+            
+            }
+            
+            
+            
+            
+
+            Float_t i, j, k, l, min3;
+            min3 = std::min(std::min(std::min(i,j), k),l);
+          
+
+            if (    
+
+            genMu0_eta != -100 && 
+            genMu0_phi != -100 &&
+
+    
+            genMu1_eta != -100 &&
+            genMu1_phi != -100 &&
+
             selMu2_eta != -100 &&
             genMu2_eta != -100 &&
             selMu2_phi != -100 &&
+            genMu2_phi != -100 &&
+    
+            genMu3_eta != -100 &&  
+            genMu3_phi != -100 ){
+
+            
+
+            
+           
+
+
+            i = ( (selMu2_eta - genMu0_eta) * (selMu2_eta - genMu0_eta) ) +  ( (selMu2_phi - genMu0_phi) * (selMu2_phi - genMu0_phi) ) ;
+            j = ( (selMu2_eta - genMu1_eta) * (selMu2_eta - genMu1_eta) ) +  ( (selMu2_phi - genMu1_phi) * (selMu2_phi - genMu1_phi) ) ;
+            k = ( (selMu2_eta - genMu2_eta) * (selMu2_eta - genMu2_eta) ) +  ( (selMu2_phi - genMu2_phi) * (selMu2_phi - genMu2_phi) ) ;
+            l = ( (selMu2_eta - genMu2_eta) * (selMu2_eta - genMu2_eta) ) +  ( (selMu2_phi - genMu2_phi) * (selMu2_phi - genMu2_phi) ) ;
+            sD_Lxy3->Fill(sqrt(min3));
+            }
+            
+            
+            
+
+
+           
+
+            Float_t m, n, o, p, min4;
+            min4 = std::min(std::min(std::min(m,n), o),p);
+
+            if (    
+
+            genMu0_eta != -100 && 
+            genMu0_phi != -100 &&
+
+
+            genMu1_eta != -100 &&
+            genMu1_phi != -100 &&
+
+  
+            genMu2_eta != -100 &&
             genMu2_phi != -100 &&
 
             selMu3_eta != -100 &&
@@ -121,65 +248,34 @@ int Matching_muons_v1(){
 
             
 
-            a = ( (selMu0_eta - genMu0_eta) * (selMu0_eta - genMu0_eta) ) +  ( (selMu0_phi - genMu0_phi) * (selMu0_phi - genMu0_phi) ) ;
-            b = ( (selMu0_eta - genMu1_eta) * (selMu0_eta - genMu1_eta) ) +  ( (selMu0_phi - genMu1_phi) * (selMu0_phi - genMu1_phi) ) ;
-            c = ( (selMu0_eta - genMu2_eta) * (selMu0_eta - genMu2_eta) ) +  ( (selMu0_phi - genMu2_phi) * (selMu0_phi - genMu2_phi) ) ;
-            d = ( (selMu0_eta - genMu3_eta) * (selMu0_eta - genMu3_eta) ) +  ( (selMu0_phi - genMu3_phi) * (selMu0_phi - genMu3_phi) ) ;
 
-
-            e = ( (selMu1_eta - genMu0_eta) * (selMu1_eta - genMu0_eta) ) +  ( (selMu1_phi - genMu0_phi) * (selMu1_phi - genMu0_phi) ) ;
-            f = ( (selMu1_eta - genMu1_eta) * (selMu1_eta - genMu1_eta) ) +  ( (selMu1_phi - genMu1_phi) * (selMu1_phi - genMu1_phi) ) ;
-            g = ( (selMu1_eta - genMu2_eta) * (selMu1_eta - genMu2_eta) ) +  ( (selMu1_phi - genMu2_phi) * (selMu1_phi - genMu2_phi) ) ;
-            h = ( (selMu1_eta - genMu3_eta) * (selMu1_eta - genMu3_eta) ) +  ( (selMu1_phi - genMu3_phi) * (selMu1_phi - genMu3_phi) ) ;
-
-            i = ( (selMu2_eta - genMu0_eta) * (selMu2_eta - genMu0_eta) ) +  ( (selMu2_phi - genMu0_phi) * (selMu2_phi - genMu0_phi) ) ;
-            j = ( (selMu2_eta - genMu1_eta) * (selMu2_eta - genMu1_eta) ) +  ( (selMu2_phi - genMu1_phi) * (selMu2_phi - genMu1_phi) ) ;
-            k = ( (selMu2_eta - genMu2_eta) * (selMu2_eta - genMu2_eta) ) +  ( (selMu2_phi - genMu2_phi) * (selMu2_phi - genMu2_phi) ) ;
-            l = ( (selMu2_eta - genMu2_eta) * (selMu2_eta - genMu2_eta) ) +  ( (selMu2_phi - genMu2_phi) * (selMu2_phi - genMu2_phi) ) ;
 
             m = ( (selMu3_eta - genMu0_eta) * (selMu3_eta - genMu0_eta) ) +  ( (selMu3_phi - genMu0_phi) * (selMu3_phi - genMu0_phi) ) ;
             n = ( (selMu3_eta - genMu1_eta) * (selMu3_eta - genMu1_eta) ) +  ( (selMu3_phi - genMu1_phi) * (selMu3_phi - genMu1_phi) ) ;
             o = ( (selMu3_eta - genMu2_eta) * (selMu3_eta - genMu2_eta) ) +  ( (selMu3_phi - genMu2_phi) * (selMu3_phi - genMu2_phi) ) ;
             p = ( (selMu3_eta - genMu3_eta) * (selMu3_eta - genMu3_eta) ) +  ( (selMu3_phi - genMu3_phi) * (selMu3_phi - genMu3_phi) ) ;
-            min= std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(std::min(a,b), c),d),e),f),g),h),k),l),m),n),o),p) ;
-          //  cout << "minimum dR =  "<< sqrt(min) << endl;
-            sD_Lxy->Fill(sqrt(min)); 
-
+            sD_Lxy4->Fill(sqrt(min4));
             }
-            else {counter1 = counter1+1;}
+
+            
+            
+           
+
+    
+            
+         //   else {counter1 = counter1+1;}
              
-            // else if ( 
-            // selMu0_eta = -100 ||
-            // genMu0_eta = -100 || 
-            // selMu0_phi = -100 ||
-            // genMu0_phi = -100 ||
+    }
 
-            // selMu1_eta = -100 ||
-            // genMu1_eta = -100 ||
-            // selMu1_phi = -100 ||
-            // genMu1_phi = -100 ||
-
-            // selMu2_eta = -100 ||
-            // genMu2_eta = -100 ||
-            // selMu2_phi = -100 ||
-            // genMu2_phi = -100 ||
-
-            // selMu3_eta = -100 ||
-            // genMu3_eta = -100 ||
-            // selMu3_phi = -100 ||
-            // genMu3_phi = -100 ){
-            //        counter1 = counter1+1; 
-            //          }
-
-
-
-  
-
-
-     }    
-
-    cout << "not matched = " << counter1 << endl;   
-    sD_Lxy->Draw();
+    cout << "not matched = " << counter1 << endl;
+    cnv1->cd();   
+    sD_Lxy1->Draw();
+    cnv2->cd();
+    sD_Lxy2->Draw();
+    cnv3->cd();
+    sD_Lxy3->Draw();
+    cnv4->cd();
+    sD_Lxy4->Draw();
 
     gStyle->SetOptFit(1);
 
@@ -188,8 +284,12 @@ int Matching_muons_v1(){
 
 
 
-    cnv->SaveAs("dR_min_Ntuples.pdf");
+    cnv1->SaveAs("dR1_min_Ntuples.pdf");
+    cnv2->SaveAs("dR2_min_Ntuples.pdf");
+    cnv3->SaveAs("dR3_min_Ntuples.pdf");
+    cnv4->SaveAs("dR4_min_Ntuples.pdf");
 
     return 0;
-}
 
+
+}
